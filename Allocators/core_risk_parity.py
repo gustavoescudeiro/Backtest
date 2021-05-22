2bce8d1776368832503fcef2b85a3d99fc827dbf
gustavo_escudeiro <gustavo.escudeiro@hotmail.com>
2021-05-22 14:08:51 -0300

    some changes

--- Allocators/core_risk_parity.py
+++ Allocators/core_risk_parity.py
@@ -115,3 +115,29 @@ def get_weight(df=None):
 
     # It returns the optimised weights
     return weights
+
+
+def get_weights_rp(signal = None, prices = None, long_and_short = False, window = 222):
+    w = window
+    returns = prices.pct_change()
+
+    if long_and_short == False:
+        long_position = {}
+        for i in signal.index:
+            all_positions = signal.loc[i, :]
+            lp = all_positions[all_positions == 1].index.to_list()
+            long_position[i] = lp
+
+        dic_long = {}
+        for i in range(w, len(returns.index) + 1):
+            df_sub = returns.iloc[(i - w + 1):i - 1]
+            df_long = df_sub[long_position[df_sub.index[0]]]
+            d_long = get_weight(df_long)
+            dic_long[df_long.index[-1]] = d_long
+
+        df_long = pd.concat(dic_long, axis=0)
+        df_long = pd.DataFrame(df_long).reset_index()
+        df_long.rename(columns={'level_0': 'date'}, inplace=True)
+        df_long = df_long.pivot(index='date', columns='ticker', values='weight')
+
+    return(df_long)
