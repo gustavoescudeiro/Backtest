9ee56354e875e665fdc080052057e04e452a58aa
gustavo_escudeiro <gustavo.escudeiro@hotmail.com>
2021-05-25 00:19:21 -0300

    fixing params kwargs

--- Allocators/handler.py
+++ Allocators/handler.py
@@ -1,11 +1,11 @@
 from Allocators.get_one_over_n_weights import *
 from Allocators.core_risk_parity import *
 
-def generate_weights(**kwargs, model = '1/n'):
-    if model == '1/n':
+def generate_weights(**kwargs):
+    if kwargs['model'] == '1/n':
         return get_weights(kwargs['df'], kwargs['long_and_short'])
 
-    elif model == 'risk_parity':
+    elif kwargs['model'] == 'risk_parity':
         return get_weights_rp(kwargs['signal'], kwargs['prices'], kwargs['long_and_short'], kwargs['window'])
 
 
