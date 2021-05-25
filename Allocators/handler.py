d774f90a4ec1304cd01bc4552638eb2dc603f6b5
gustavo_escudeiro <gustavo.escudeiro@hotmail.com>
2021-05-25 00:13:02 -0300

    handler changes

--- Allocators/handler.py
+++ Allocators/handler.py
@@ -1,4 +1,11 @@
 from Allocators.get_one_over_n_weights import *
 from Allocators.core_risk_parity import *
 
-generate_weights
+def generate_weights(**kwargs, model = '1/n'):
+    if model == '1/n':
+        return get_weights(kwargs['df'], kwargs['long_and_short'])
+
+    elif model == 'risk_parity':
+        return get_weights_rp(kwargs['signal'], kwargs['prices'], kwargs['long_and_short'], kwargs['window'])
+
+
